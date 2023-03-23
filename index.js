316e666504af0417fa31c8a557a8edeeaaa1a705
Paul <80938413+accellier@users.noreply.github.com>
2023-03-23 14:52:38 +1100

    Update index.js
    Updated to accommodate recent API changes and now uses GPT 3.5 turbo.

--- index.js
+++ index.js
@@ -1,69 +1,89 @@
+/*
+INSTRUCTIONS: Go to https://platform.openai.com/account/api-keys and get an API key.
+Copy it, then put it below where it says YOUR API KEY HERE
+
+WARNING: This was only intended for personal demonstration purposes of what's possible with the ChatGPT API
+Do not use this code for any real-world production purposes.
+Putting your API key here will expose it to any user who inspects the code's source.
+For production purposes you will need to use a server-side language like Node.js or PHP to perform the requests then send the bot response back to the client.
+*/
+const API_KEY = 'YOUR API KEY HERE';
+
 document.addEventListener("DOMContentLoaded", () => {
-  const inputField = document.getElementById("input");
-  inputField.addEventListener("keydown", (e) => {
-    if (e.code === "Enter") {
-      let input = inputField.value;
-      inputField.value = "";
-      addChat(input);
-    }
-  });
+    const inputField = document.getElementById("input");
+    inputField.addEventListener("keydown", (e) => {
+        if (e.code === "Enter") {
+            let input = inputField.value;
+            inputField.value = "";
+            addChat(input);
+        }
+    });
 });
 
 async function addChat(input) {
-  const messagesContainer = document.getElementById("messages");
+    const messagesContainer = document.getElementById("messages");
 
-  let userDiv = document.createElement("div");
-  userDiv.id = "user";
-  userDiv.className = "user response";
-  userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
-  messagesContainer.appendChild(userDiv);
+    let userDiv = document.createElement("div");
+    userDiv.id = "user";
+    userDiv.className = "user response";
+    userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
+    messagesContainer.appendChild(userDiv);
 
-  let botDiv = document.createElement("div");
-  let botImg = document.createElement("img");
-  let botText = document.createElement("span");
-  botDiv.id = "bot";
-  botImg.src = "bot-mini.png";
-  botImg.className = "avatar";
-  botDiv.className = "bot response";
-  botText.innerText = "Typing...";
-  botDiv.appendChild(botText);
-  botDiv.appendChild(botImg);
-  messagesContainer.appendChild(botDiv);
-  // Keep messages at most recent
-  messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+    let botDiv = document.createElement("div");
+    let botImg = document.createElement("img");
+    let botText = document.createElement("span");
+    botDiv.id = "bot";
+    botImg.src = "bot-mini.png";
+    botImg.className = "avatar";
+    botDiv.className = "bot response";
+    botText.innerText = "Responding...";
+    botDiv.appendChild(botText);
+    botDiv.appendChild(botImg);
+    messagesContainer.appendChild(botDiv);
+    // Keep messages at most recent
+    messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
 
-  //This calls the function to make the request.
-  let cgpt = await getChatGPT(input);
-  //Set the botText innerText to the result from the getChatGPT function
-  botText.innerText = cgpt.botResponse;
+    //This calls the function to make the request.
+    let cgpt = await getChatGPT(input);
+
+    //Set the botText innerText to the result from the getChatGPT function
+    botText.innerText = cgpt.botResponse;
 }
 
-async function getChatGPT(prompt){
+async function getChatGPT(prompt) {
+    //Read the docs at https://platform.openai.com/docs/introduction for info on how to adjust model, temperature, tokens etc below.
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${API_KEY}`
+            },
+            body: JSON.stringify({
+                "model": "gpt-3.5-turbo",
+                "messages": [{
+                    "role": "user",
+                    "content": prompt
+                }],
+                "temperature": 0.7
+            }
+            )
+        });
+        if (response.status === 401) {
+            return {
+                'botResponse': 'There was a 401 error from OpenAI. Please check your API key is valid'
+            };
+        } else {
+            const json = await response.json();
+            return {
+                'botResponse': json.choices[0].message.content.trimStart()
+            };
+        }
 
-  //Change 'xx-xxxx' to your own API Key provided by openai.com
-  //This was for prototyping only - DO NOT store your API key here for any production use as any user can see it.
-  //For production you would want a more sophisticated approach to call the API from the server not the client (i.e. through NodeJS or PHP)
-  //Read the docs at https://beta.openai.com/docs/introduction for info on how to adjust model, temperature, tokens etc below.
-  const response = await fetch('https://api.openai.com/v1/completions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer xx-xxxx'
-    },
-    body: JSON.stringify({
-      'model': 'text-davinci-003',
-      'prompt': prompt,
-      'temperature': 0,
-      'max_tokens': 7
-    })
-  });
-  const json = await response.json();
-
-  //response ID not used but could be useful later
-  //seems to always add a /n newline character to response hence trimStart()
-  return {
-    'botResponse': json.choices[0].text.trimStart(),
-    'id': json.id
-  };
+    } catch (error) {
+        console.log('There was an error talking to ChatGPT: ', error);
+        return {
+            'botResponse': 'Something went wrong... ' + error
+        };
+    }
 }
-
